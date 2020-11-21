d38841798c04b676695da956173a63e7fa32bf71
rick2785 <rjhrabowskie@gmail.com>
2020-11-21 11:47:30 -0600

    Initial Commit

---
+++ FoodOrdering/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodOrdering
+//
+//  Created by RJ Hrabowskie on 11/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodOrdering/FoodOrderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodOrderingApp.swift
+//  FoodOrdering
+//
+//  Created by RJ Hrabowskie on 11/21/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodOrderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
